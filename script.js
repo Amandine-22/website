1f8d6fc6b1560fc3c44ec8e9e859b1d050687c57
Amandine-22 <amandine.benichou6@gmail.com>
2025-12-29 23:02:29 +0100

    creating burger menu

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const burgerMenu = document.querySelector('.burger-menu');
+const navMenu = document.querySelector('.nav-menu');
+const overlay = document.querySelector('.overlay');
+
+
+burgerMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    burgerMenu.classList.toggle('active');
+    overlay.classList.toggle('active');
+});
+
+// Fermer le menu en cliquant sur l'overlay
+overlay.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+    burgerMenu.classList.remove('active');
+    overlay.classList.remove('active');
+});
+
+// Fermer le menu en cliquant sur un lien
+document.querySelectorAll('.nav-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.remove('active');
+        burgerMenu.classList.remove('active');
+        overlay.classList.remove('active');
+    });
+});
